956d50543ca48460bf13f167c267d7319af235ca
Silal <111568366+Silal123@users.noreply.github.com>
2025-03-21 22:20:52 +0100

    u

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url && changeInfo.url.includes('github.com') && changeInfo.url.includes('/settings')) {
-        console.log("changed" + changeInfo.url);
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ['scripts/fill_confirm.js']
-        });
-    }
-});
--- scripts/fill_confirm.js
+++ scripts/fill_confirm.js
@@ -1,11 +1,12 @@
-(function () {
+function exec() {
+    console.debug("loaded delete listener");
     const deleteButton = document.getElementById('dialog-show-repo-delete-menu-dialog');
     let loopId;
-
+    
     if (deleteButton) {
         deleteButton.addEventListener('click', () => {
             console.debug('triggered delete function');
-
+    
             const closeButton = document.querySelector('[data-close-dialog-id="repo-delete-menu-dialog"]');
             if(closeButton) {
                 closeButton.addEventListener('click', () => {
@@ -13,23 +14,23 @@
                     console.debug('cleared Loop');
                 });
             }
-
+    
             loopId = setInterval(() => {
                 console.debug(loopId);
                 const verificationField = document.getElementById('verification_field');
-
+    
                 if (verificationField) {
                     console.debug('f')
                     const args = window.location.href.replace('https://', '').split('/');
                     const repo = args[1] + '/' + args[2];
-
+    
                     verificationField.focus();
                     verificationField.setRangeText(repo);
                     verificationField.dispatchEvent(new Event("input", { bubbles: true }));
-
+    
                     clearInterval(loopId);
                 }
             }, 100);
         });
     }
-})();
+}
